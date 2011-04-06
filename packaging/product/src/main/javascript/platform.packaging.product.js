012d6e107491d3a165a63de23ff30234a26b832a
Peter Nedonosko <pnedonosko@exoplatform.com>
2011-04-06 08:26:03 +0000

    PLF-1222 upgrade of dependencies to CJR 1.14-CR2

--- packaging/product/src/main/javascript/platform.packaging.product.js
+++ packaging/product/src/main/javascript/platform.packaging.product.js
@@ -169,7 +169,7 @@ function getProduct(version) {
   product.removeDependency(new Project("commons-digester", "commons-digester", "jar", "1.6"));
   product.removeDependency(new Project("xstream", "xstream", "jar", "1.0.2"));
   product.removeDependency(new Project("ical4j", "ical4j", "jar", "0.9.20"));
-  product.removeDependency(new Project("commons-lang", "commons-lang", "jar", "2.3"));
+//  product.removeDependency(new Project("commons-lang", "commons-lang", "jar", "2.3"));
   
   /* remove extensions config . We don't need them because PLF declares a global container config in exo.platform.config */
   product.removeDependency(new Project("org.exoplatform.ecms", "exo-ecms-packaging-ecmdemo-config", "jar", wcm.version));
